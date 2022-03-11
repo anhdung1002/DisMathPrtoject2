e671a553edb6a66fbec63d34c6f71d3517e364d0
Iron Man <84199729+anhdung1002@users.noreply.github.com>
2022-03-11 09:00:24 -0700

    add config json

--- TruthTable.java
+++ TruthTable.java
@@ -1,16 +1,30 @@
 public class TruthTable {
     static Logger log = new Logger();
 
-    static public String[][] setup(String [] vars){
+    static public String[][] setup(int numOfArgs){
 
-        int numOfPeople = vars.length;
-        int numOfRow = (int) Math.pow(2, numOfPeople);
-        // Create a 2x2 matrix
-        String [][] myTable = new String [numOfPeople][numOfRow];
+        // Calculate the number of row
+        int numOfRow = (int) Math.pow(2, numOfArgs);
+        // Create an array with Pn
+        String [] pSequence = new String [numOfArgs];
+        for(int p = 0; p < numOfArgs; p++){
+            pSequence[p] = "p"+(p+1);
+            log.print(pSequence[p]);
+        }
+
+
+
+
+
+
+
+
+        // Create a 2-dimensional matrix
+        String [][] myTable = new String [numOfArgs][numOfRow];
 
         // for(int i = numOfPeople -1 ; i >= 0; i--){
-            for(int i = 0 ; i < numOfPeople; i++){
-            int groupSize = (int) Math.pow(2, numOfPeople - 1 -i);
+            for(int i = 0 ; i < numOfArgs; i++){
+            int groupSize = (int) Math.pow(2, numOfArgs - 1 -i);
             int tracker = groupSize;
             String valToInsert = "T";
             for(int j = 0; j < numOfRow; j++){
@@ -30,17 +44,17 @@ public class TruthTable {
             }
         }
         // Print out the truth table //
-        log.print("TruthTable");
-        for(int z = 0 ; z < numOfPeople; z++){
-            System.out.print(vars[z] + "\t");
-        }
-        System.out.println();
-        for(int m = 0; m < numOfRow; m++){
-            for(int n = 0; n < numOfPeople; n++){
-                System.out.print(myTable[n][m] + "\t");
-            }
-            System.out.println();
-        }
+        // log.print("TruthTable");
+        // for(int z = 0 ; z < numOfPeople; z++){
+        //     System.out.print(vars[z] + "\t");
+        // }
+        // System.out.println();
+        // for(int m = 0; m < numOfRow; m++){
+        //     for(int n = 0; n < numOfPeople; n++){
+        //         System.out.print(myTable[n][m] + "\t");
+        //     }
+        //     System.out.println();
+        // }
 
         return myTable;
     }
